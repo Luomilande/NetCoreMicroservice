6057d9f183122dd6da494d8d34f26c8fe95050ce
CoCo <merb001@outlook.com>
2020-10-31 04:20:55 +0800

    添加项目文件。

---
+++ Order.API/Controllers/OrdersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Order.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class OrdersController : Controller
+    {
+        private readonly ILogger<OrdersController> _logger;
+        private readonly IConfiguration _configuration;
+        public OrdersController(ILogger<OrdersController>logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string result = $"【订单服务】{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}——"+
+                $"{Request.HttpContext.Connection.LocalIpAddress}:{Request.HttpContext.Connection.LocalPort}";
+            return Ok(result);
+        }
+    }
+}
---
+++ Web.MVC/Helper/ServiceHelper.cs
@@ -0,0 +1,35 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.MVC.Helper
+{
+    public class ServiceHelper:IServiceHelper
+    {
+        public async Task<string> GetOrder()
+        {
+            string[] serviceUrls = { "http://localhost:9060", "http://localhost:9061", "http://localhost:9062" };//订单服务的地址，可以放在配置文件或者数据库等等...
+
+            //每次随机访问一个服务实例
+            var Client = new RestClient(serviceUrls[new Random().Next(0, 3)]);
+            var request = new RestRequest("/order", Method.GET);
+
+            var response = await Client.ExecuteAsync(request);
+            return response.Content;
+        }
+
+        public async Task<string> GetProduct()
+        {
+            string[] serviceUrls = { "http://localhost:9050", "http://localhost:9051", "http://localhost:9052" };//产品服务的地址，可以放在配置文件或者数据库等等...
+
+            //每次随机访问一个服务实例
+            var Client = new RestClient(serviceUrls[new Random().Next(0, 3)]);
+            var request = new RestRequest("/product", Method.GET);
+
+            var response = await Client.ExecuteAsync(request);
+            return response.Content;
+        }
+    }
+}
